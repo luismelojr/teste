b76355dafbc6dca4bcc2c7a8e00e7930bef32ce0
Luis Henrique <junimhs1996@gmail.com>
2024-03-07 12:03:24 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 import html2canvas from 'html2canvas';
 import { ref } from 'vue';
+// import fetch from 'node-fetch'
+// const response = await fetch('https://storage.googleapis.com/assets.dev.cuco.info/logos/firstbox/adium.png');
+// image_base64 = (await response.buffer()).toString('base64');
 
+// const imageRef = ref(`data:image/png;base64,${image_base64}`);
 const imageRef = ref('https://storage.googleapis.com/assets.dev.cuco.info/logos/firstbox/adium.png');
+// const imageRef = ref('https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png');
 async function printScreenAndDownload() {
   const canvas = await html2canvas(
     document.querySelector(`#printAqui`),
