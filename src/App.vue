34014fd4929c37e42ac77668175f9b8122712d28
Luis Henrique <junimhs1996@gmail.com>
2024-03-07 11:21:12 -0300

    teste

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import html2canvas from 'html2canvas';
+async function printScreenAndDownload() {
+  const canvas = await html2canvas(
+    document.querySelector(`#printAqui`),
+    {
+      logging: true,
+      letterRendering: 1,
+      useCORS: true
+    }
+  )
+
+  const link = document.createElement('a')
+  link.href = canvas.toDataURL('image/png')
+  link.download = 'cupom.png'
+
+  document.body.appendChild(link)
+  link.click()
+
+  document.body.removeChild(link)
+}
+</script>
+
+<template>
+  <div>
+    <h1>Teste</h1>
+    <button @click="printScreenAndDownload">Print</button>
+    <div id="printAqui">
+      <h2>Print me</h2>
+      <img src="https://storage.googleapis.com/assets.dev.cuco.info/logos/firstbox/adium.png" alt=""
+        style="width: 320px;" crossorigin="true">
+    </div>
+  </div>
+</template>
